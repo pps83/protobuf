e3f9294ee3e7a1a749a25803f9f197a84def3b63
Pavel P <pavlov.pavel@gmail.com>
2026-02-27 09:40:28 +0200

    Auto-generate files with generate_descriptor_proto.sh

--- src/google/protobuf/cpp_features.pb.cc
+++ src/google/protobuf/cpp_features.pb.cc
@@ -21,7 +21,10 @@
 
 // Must be included last.
 #include "google/protobuf/port_def.inc"
+#ifndef PROTOBUF_PRAGMA_INIT_SEG_USED
+#define PROTOBUF_PRAGMA_INIT_SEG_USED
 PROTOBUF_PRAGMA_INIT_SEG
+#endif
 namespace _pb = ::google::protobuf;
 namespace _pbi = ::google::protobuf::internal;
 namespace _fl = ::google::protobuf::internal::field_layout;
@@ -74,10 +77,10 @@ const ::uint32_t
 };
 
 static const ::_pbi::MigrationSchema
-    schemas[] ABSL_ATTRIBUTE_SECTION_VARIABLE(protodesc_cold) = {
+    schemas_google_2fprotobuf_2fcpp_5ffeatures_2eproto[] ABSL_ATTRIBUTE_SECTION_VARIABLE(protodesc_cold) = {
         {0, sizeof(::pb::CppFeatures)},
 };
-static const ::_pb::Message* PROTOBUF_NONNULL const file_default_instances[] = {
+static const ::_pb::Message* PROTOBUF_NONNULL const file_default_instances_google_2fprotobuf_2fcpp_5ffeatures_2eproto[] = {
     &::pb::_CppFeatures_default_instance_._instance,
 };
 const char descriptor_table_protodef_google_2fprotobuf_2fcpp_5ffeatures_2eproto[] ABSL_ATTRIBUTE_SECTION_VARIABLE(
@@ -115,8 +118,8 @@ PROTOBUF_CONSTINIT const ::_pbi::DescriptorTable descriptor_table_google_2fproto
     descriptor_table_google_2fprotobuf_2fcpp_5ffeatures_2eproto_deps,
     1,
     1,
-    schemas,
-    file_default_instances,
+    schemas_google_2fprotobuf_2fcpp_5ffeatures_2eproto,
+    file_default_instances_google_2fprotobuf_2fcpp_5ffeatures_2eproto,
     TableStruct_google_2fprotobuf_2fcpp_5ffeatures_2eproto::offsets,
     file_level_enum_descriptors_google_2fprotobuf_2fcpp_5ffeatures_2eproto,
     file_level_service_descriptors_google_2fprotobuf_2fcpp_5ffeatures_2eproto,
